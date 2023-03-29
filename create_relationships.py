326c4f45aeffec397bd01793a7f135b5656063da
BlakeDarrington <122818179+BlakeDarrington@users.noreply.github.com>
2023-03-29 16:38:01 -0400

    finished lab 8

---
+++ create_relationships.py
@@ -0,0 +1,89 @@
+"""
+Description:
+ Creates the relationships table in the Social Network database
+ and populates it with 100 fake relationships.
+
+Usage:
+ python create_relationships.py
+"""
+import os
+import inspect
+import sqlite3
+from faker import Faker
+from datetime import datetime
+from random import randint, choice
+
+# Determine the path of the database
+db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'social_network.db')
+
+def main():
+    create_relationships_table()
+    populate_relationships_table()
+    fake = Faker()
+
+def create_relationships_table():
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    """Creates the relationships table in the DB"""
+    create_relationships_tbl_query = """
+    CREATE TABLE IF NOT EXISTS relationships
+    (
+    id INTEGER PRIMARY KEY,
+    person1_id INTEGER NOT NULL,
+    person2_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    FOREIGN KEY (person1_id) REFERENCES people (id),
+    FOREIGN KEY (person2_id) REFERENCES people (id)
+    );
+    """
+    cur.execute(create_relationships_tbl_query)
+    con.commit()
+    con.close()
+    return
+
+def populate_relationships_table():
+    fake = Faker("en_CA")
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+    """Adds 100 random relationships to the DB"""
+    add_relationship_query = """
+    INSERT INTO relationships
+    (
+    person1_id,
+    person2_id,
+    type,
+    start_date
+    )
+    VALUES (?, ?, ?, ?);
+    """
+    for i in range(100):
+        person1_id = randint(1,200)
+        person2_id = randint(1,200)
+        while person2_id == person1_id:
+            person2_id = randint(1,200)
+
+        
+        # Randomly select a relationship type
+        rel_type = choice(('friend', 'spouse', 'partner', 'relative'))
+
+        # Randomly select a relationship start date between now and 50 years ago
+        start_date = fake.date_between(start_date='-50y', end_date='today')
+
+        # Create tuple of data for the new relationship
+        new_relationship = (person1_id, person2_id, rel_type, start_date)
+
+        # Add the new relationship to the DB
+        cur.execute(add_relationship_query, new_relationship)
+    con.commit()
+    con.close()
+
+
+    
+
+
+    # TODO: Function body
+    return 
+
+if __name__ == '__main__':
+   main()
